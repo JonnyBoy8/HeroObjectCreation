34347039d2f16f2676e21b4bb23e4d59b54aa29d
JonnyBoy8 <jtha99@uw.edu>
2023-04-07 15:30:34 -0700

    assets & script created for hero

---
+++ Hero/Assets/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public bool KeyBoardMovement = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!KeyBoardMovement) //if m hasnt been pressed
+        {
+
+        }
+        else //if m is pressed
+        {
+
+        }
+    }
+}
