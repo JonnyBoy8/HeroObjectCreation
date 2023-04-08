c1603fd24fc4b585602a8ba9c8a086be2b27a8ba
JonnyBoy8 <jtha99@uw.edu>
2023-04-07 23:04:11 -0700

    restarted keyboard mode

--- Hero/Assets/HeroMovement.cs
+++ Hero/Assets/HeroMovement.cs
@@ -13,6 +13,10 @@ public class HeroMovement : MonoBehaviour
     [SerializeField]
     public float kHeroRotateSpeed;
 
+    //egg variables
+    public Transform EggSpawn;
+    public GameObject EggPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +47,11 @@ public class HeroMovement : MonoBehaviour
             float angle = rotateInput * (kHeroRotateSpeed * Time.smoothDeltaTime);
 
             transform.Rotate(transform.forward, angle);
+
+            if(Input.GetKey(KeyCode.Space))
+            {
+
+            }
         }
         else
         {
@@ -53,53 +62,20 @@ public class HeroMovement : MonoBehaviour
                 return;
             }
 
-            kHeroRotateSpeed = 360f;
-            //either A,D or Left,Right arrows
-            //right arrow = 1
-            //left arrow = -1
-            float horizontalInput = Input.GetAxis("Horizontal");
-            if(Input.GetKey(KeyCode.W))
-            {
-                kHeroSpeed += .03f;
-                Debug.Log("increasing speed");
-            }
-
-            if(Input.GetKey(KeyCode.S))
-            {
-                kHeroSpeed -= .03f;
-                Debug.Log("decreasing speed");
-            }
-
-            //either W,S, or Up,Down arrows
-            float verticalInput = Input.GetAxis("Vertical");
-
-            // transform.position += verticalInput * transform.up * (kHeroSpeed * Time.smoothDeltaTime);
-            // transform.position += horizontalInput * transform.right * (kHeroSpeed * Time.smoothDeltaTime);
-
-            //direction we want character to move in
-            Vector2 moveDirection = new Vector2(horizontalInput, verticalInput);
-
-            //store orginal magnitude in a range of only -1 to 1
-            float OGMagnitude = Mathf.Clamp01(moveDirection.magnitude);
-
-            //different inputs can lead to values over or less than 1. normalize so that
-            //the input will always be -1 or 1
-            moveDirection.Normalize();
-
-            //actually moving the character
-            transform.Translate(moveDirection * kHeroSpeed * OGMagnitude * Time.deltaTime, Space.World);
-
-            //check if character is moving
-            if(moveDirection != Vector2.zero)
-            {
-                Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, moveDirection);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, kHeroRotateSpeed * Time.deltaTime);
-            }
+            KeyBoardSettings();
         }
     }
 
-    void KeyBoardSettings()
+    private void KeyBoardSettings()
     {
 
+    }
+
+    private void BulletSpawn()
+    {
+        if((Input.GetKey(KeyCode.Space)))
+        {
+
+        }
     }
 }
