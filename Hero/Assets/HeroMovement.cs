fc45db053743617f60f27750a3409ee16452decc
JonnyBoy8 <jtha99@uw.edu>
2023-04-07 23:57:37 -0700

    redid keyboard mode, changed rotate in arrow

--- Hero/Assets/HeroMovement.cs
+++ Hero/Assets/HeroMovement.cs
@@ -42,7 +42,7 @@ public class HeroMovement : MonoBehaviour
             transform.position = Camera.main.ScreenToWorldPoint(pos);
 
             //AD rotate
-            kHeroRotateSpeed = 90f;
+            kHeroRotateSpeed = 45f; //change to per seconds
             float rotateInput = Input.GetAxis("Horizontal");
             float angle = rotateInput * (kHeroRotateSpeed * Time.smoothDeltaTime);
 
@@ -68,14 +68,35 @@ public class HeroMovement : MonoBehaviour
 
     private void KeyBoardSettings()
     {
+        //kHeroSpeed = 20f;
+        kHeroRotateSpeed = 45f; 
 
+        transform.position += transform.up * (kHeroSpeed * Time.smoothDeltaTime);
+        if(Input.GetKey(KeyCode.W))
+        {
+            kHeroSpeed += .025f;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            kHeroSpeed -= .025f;
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.position += horizontalInput * transform.right * (kHeroSpeed * Time.smoothDeltaTime);
+
+        float angle = (-1f * horizontalInput) * (kHeroRotateSpeed * Time.smoothDeltaTime);
+        //transform.rotate.z = angle;
+        transform.Rotate(transform.forward, angle);
+
+        if((Input.GetKey(KeyCode.Space)))
+        {
+            Debug.Log("Egg Spawn");
+        }
     }
 
     private void BulletSpawn()
     {
-        if((Input.GetKey(KeyCode.Space)))
-        {
 
-        }
     }
 }
